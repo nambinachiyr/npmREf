df02e28201806b6a9a86374bc313f48dc846e1a7
Nambi Nachiyar <nambi2731@gmail.com>
2025-09-09 20:09:17 +0530

    Get Todo By ID--with Error Handling

--- controler.js
+++ controler.js
@@ -1,13 +1,23 @@
 const notes = require('./data')
 const controler = {
-    getAllTodos:(req,res)=>{
-      res.json(notes)
+    getAllTodos:async(req,res)=>{
+      try{
+        res.json(notes)
+      }
+      catch{
+        res.status(500).json({message:"Todo not Fetching"})
+      }
     },
     getTodoById:async(req,res)=>{
       try{
         const todoId = req.params.id
         const todo = notes.find(todo=>todo.id===todoId)
-        res.json(todo)
+        if(todo){
+          res.json(todo)
+        }
+        else{
+          res.status(404).json({message:`Todo id - ${todoId} is not in there`})
+        }
         // console.log(todoId)
       }
       catch{
